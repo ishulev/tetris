e3b2bc4f9c010cb3deb49ac757ed8b4c59bb9310
Ivan Shulev <ivan.venelinov.shulev@gmail.com>
2016-09-15 12:16:35 +0300

    added jQuery

--- src/app.js
+++ src/app.js
@@ -1,14 +1,11 @@
-(function(){
+(function($){
 	'use strict';
 	function getRandomInt(min, max) {
 		return Math.floor(Math.random() * (max - min + 1)) + min;
 	}
-	window.onload = function() {
-		console.log('loaded!');
-		var startingSquare = getRandomInt(1, 12);
-		var playfield = document.getElementById('playfield');
-		setTimeout(function(){
-			console.log(playfield);
-		}, 1000);
-	}
-})();
+	var startingSquare = getRandomInt(1, 12);
+	$(function() {
+		var playfield = $('#playfield');
+		console.log(playfield);
+	});
+})(jQuery);
